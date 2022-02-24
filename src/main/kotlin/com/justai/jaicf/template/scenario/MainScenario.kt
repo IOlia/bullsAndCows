432921eda23babec1021f1491ee96a28734dab3e
Yulia Kiseleva <yk@digintel.ru>
2022-02-24 14:09:20 +0300

    Expand scenario

--- src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
+++ src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
@@ -12,6 +12,7 @@ import com.justai.jaicf.context.BotContext
 //import com.justai.jaicf.context.ExecutionContext
 
 val random = Random()
+var intendedNumber = ""
 fun rand(from: Int, to: Int) : Int {
     return random.nextInt(to - from) + from
 }
@@ -29,7 +30,7 @@ fun formIntendedNumber(): String {
 }
 
 fun checkLetterRepetition(number: String): Boolean {
-    val array = number.split("")
+    val array: List<String> = number.map { it.toString() }
     var otherArray = ArrayList<String>()
 
     for (x in array.indices) {
@@ -38,10 +39,47 @@ fun checkLetterRepetition(number: String): Boolean {
         } else {
             return true
         }
-
-
     }
     return false
+}
+
+fun conform(word: String, number: Number): String {
+    if (word == "бык") {
+        return when (number) {
+            0 -> "быков"
+            1 -> "бык"
+            else -> "быка"
+        }
+    }
+
+    if (word == "корова") {
+        return when (number) {
+            0 -> "коров"
+            1 -> "корова"
+            else -> "коровы"
+        }
+    }
+    return ""
+}
+
+fun checkBullsAndCows(intendedNumber: String, clientNumber: String): String {
+
+    var bulls = 0
+    var cows = 0
+
+    for (x in 0..3) {
+        if (intendedNumber.contains(clientNumber[x])) {
+            if (clientNumber[x] == intendedNumber[x]) {
+                bulls ++
+            } else {
+                cows ++
+            }
+        }
+    }
+    //val answer = "В твоем числе: $bulls" + conform("бык", bulls) + "и $cows" + conform("корова", cows) + "."
+
+    return "В твоем числе: $bulls ${conform("бык", bulls)} и $cows ${conform("корова", cows)}."
+
 }
 
 val mainScenario = Scenario {
@@ -74,8 +112,10 @@ val mainScenario = Scenario {
                 action {
                     reactions.say("Напомню правила: я загадываю 4-значное число с неповторяющимися цифрами. Твоя задача - угадать его. В ответ я скажу, сколько в твоем числе быков (сколько цифр ты угадал вплоть до позиции) и сколько коров (цифр без совпадения позиций). Например: я загадал число 3219, ты пробуешь отгадать и называешь 2310. Это будет один бык (цифра 1 угадана с позицией) и две коровы (цифры 2 и 3 угаданы без совпадения позиций). Если тебе надоест играть, просто скажи Стоп. Итак, я загадал число. Попробуй отгадать?!")
                     //attempts = 0
-                    context.session["intendedNumber"] = formIntendedNumber()
-                    reactions.say("${context.session["intendedNumber"]}")
+                    //context.session["intendedNumber"] = formIntendedNumber()
+                    intendedNumber = formIntendedNumber()
+                    //reactions.say("${context.session["intendedNumber"]}")
+                    reactions.say(intendedNumber)
                 }
 
                 state("getNumber") {
@@ -89,6 +129,7 @@ val mainScenario = Scenario {
                         //} else {
                         //    attempts += 1
                         //}
+                        //var x = request
                         var number : String = request.input
                         if (number.length < 4) {
                             reactions.say("В твоем числе меньше четырех цифр. Попробуй еще раз!")
@@ -96,16 +137,14 @@ val mainScenario = Scenario {
                             reactions.say("В твоем числе больше четырех цифр. Давай еще раз!")
                         } else if (checkLetterRepetition(number)) {
                             reactions.say("В твоем числе не должно быть повторяющихся чисел, попробуй другое число!")
-                        } else if (number == context.session["intendedNumber"]) {
+                        } else if (number == intendedNumber) {
                             reactions.say("Ура, ты отгадал!")
                         //    reactions.say("Попытки: $attempts")
                             reactions.buttons("Сыграть еще раз" to "/start/letsPlay", "Хватит играть" to "/start/dontWantToPlay")
                         } else {
-                            reactions.say("Попробуй еще")
-                        }//else {
-                        //    val answer: String = checkBullsAndCows(intendedNumber, number)
-                        //    reactions.say(answer)
-                        //}
+                            var answer: String = checkBullsAndCows(intendedNumber, number)
+                            reactions.say(answer)
+                        }
                     }
                 }
             }
